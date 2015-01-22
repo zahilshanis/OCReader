37ce7e74ae198783e45ef0998291c9b4a53b91fe
zahilshanis <zahilshanis@gmail.com>
2015-01-22 15:42:01 +0530

    Logic Correct. Something wrong with http library

--- app/src/main/java/com/ocreaderindia/ocreader/MainActivity.java
+++ app/src/main/java/com/ocreaderindia/ocreader/MainActivity.java
@@ -1,8 +1,14 @@
 package com.ocreaderindia.ocreader;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
@@ -13,11 +19,26 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -33,77 +54,256 @@ public class MainActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i("up","created");
-                File photoFile = dispatchTakePictureIntent();
-                //new UploadToServer().execute(photoFile);
+                selectImage();
             }
         });
     }
+    private String selectedImagePath = "";
+    final private int PICK_IMAGE = 1;
+    final private int CAPTURE_IMAGE = 2;
+    private String imgPath;
 
-    static final int REQUEST_TAKE_PHOTO = 1;
+    private void selectImage() {
 
-    public File dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                //Dothis
-                       }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                //File photoBinary = null;
-                //photoBinary = convertToBinary(photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                        Uri.fromFile(photoFile));
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-                Log.i("pic","taken!");
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+// builder.setTitle("Choose Image Source");
+        builder.setItems(new CharSequence[] { "Take a Photo",
+                        "Choose from Gallery" },
+                new DialogInterface.OnClickListener() {
 
-                return photoFile;
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                Intent intent1 = new Intent(
+                                        MediaStore.ACTION_IMAGE_CAPTURE);
+                                intent1.putExtra(MediaStore.EXTRA_OUTPUT,
+                                        setImageUri());
+                                startActivityForResult(intent1, CAPTURE_IMAGE);
+                                break;
+                            case 1:
+                                // GET IMAGE FROM THE GALLERY
+                                Intent intent = new Intent();
+                                intent.setType("image/*");
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                                startActivityForResult(
+                                        Intent.createChooser(intent, ""),
+                                        PICK_IMAGE);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                });
+        builder.show();
+    }
+
+
+    public Uri setImageUri() {
+
+        File file = new File(Environment.getExternalStorageDirectory(), "image" + new     Date().getTime() + ".png");
+        Uri imgUri = Uri.fromFile(file);
+        this.imgPath = file.getAbsolutePath();
+        return imgUri;
+    }
+
+
+    public String getImagePath() {
+        return imgPath;
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        if (resultCode != Activity.RESULT_CANCELED) {
+            if (requestCode == PICK_IMAGE) {
+                selectedImagePath = getAbsolutePath(data.getData());
+                System.out.println("path" + selectedImagePath);
+                imageView.setImageBitmap(decodeFile(selectedImagePath));
+                new PostDataAsyncTask().execute();
+
+            } else if (requestCode == CAPTURE_IMAGE) {
+                selectedImagePath = getImagePath();
+                System.out.println("path" + selectedImagePath);
+                imageView.setImageBitmap(decodeFile(selectedImagePath));
+                new PostDataAsyncTask().execute();
+
+
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
             }
+        }
+    }
+
+
+    public Bitmap decodeFile(String path) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(path, o);
+            // The new size we want to scale to
+            final int REQUIRED_SIZE = 70;
+
+            // Find the correct scale value. It should be the power of
+            // 2.
+            int scale = 1;
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE
+                    && o.outHeight / scale / 2 >= REQUIRED_SIZE)
+                scale *= 2;
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeFile(path, o2);
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent takePictureIntent) {
-        if (requestCode == 1) {
-            Bitmap photo = (Bitmap) takePictureIntent.getExtras().get("data");
-            ImageView test = (ImageView) findViewById(R.id.imageView);
-            test.setImageBitmap(photo);
 
+    public String getAbsolutePath(Uri uri) {
+        String[] projection = { MediaStore.MediaColumns.DATA };
+
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if (cursor != null) {
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else
+            return null;
+    }
+
+    public class PostDataAsyncTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // do stuff before posting data
+
+            Toast.makeText(MainActivity.this,"File Posted",Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
             try {
-                FileOutputStream out = new FileOutputStream("filename");
-                photo.compress(Bitmap.CompressFormat.JPEG, 90, out);
+
+                // 1 = post text data, 2 = post file
+                int actionChoice = 2;
+
+                // post a text data
+                if(actionChoice==1){
+                    postText();
+                }
+
+                // post a file
+                else{
+                    postFile();
+                }
+
+            } catch (NullPointerException e) {
+                e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String lenghtOfFile) {
+            // do stuff after posting data
         }
     }
 
-    String mCurrentPhotoPath;
-    String imageFileName;
+    // this will post our text data
+    private void postText(){
+        try{
+            // url where the data will be posted
+            String postReceiverUrl = "http://10.22.29.41/android_upload.php";
+            Log.v("TAG", "postURL: " + postReceiverUrl);
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+            // HttpClient
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        imageFileName = imageFileName + ".jpg";
+            HttpClient httpClient = new DefaultHttpClient();
 
-        Log.i("path", mCurrentPhotoPath);
-        Log.i("fname",imageFileName);
-        return image;
+            // post header
+
+            HttpPost httpPost = new HttpPost(postReceiverUrl);
+
+            // add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("firstname", "Mike"));
+            nameValuePairs.add(new BasicNameValuePair("lastname", "Dalisay"));
+            nameValuePairs.add(new BasicNameValuePair("email", "mike@testmail.com"));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // execute HTTP post request
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity resEntity = response.getEntity();
+
+            if (resEntity != null) {
+
+                String responseStr = EntityUtils.toString(resEntity).trim();
+                Log.v("TAG", "Response: " +  responseStr);
+
+                // you can add an if statement here and do other actions based on the response
+            }
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+    // will post our text file
+    private void postFile(){
+        try{
+
+            // the file to be posted
+            String textFile = Environment.getExternalStorageDirectory() + "/sample.txt";
+            Log.v("TAG", "textFile: " + textFile);
+
+            // the URL where the file will be posted
+            String postReceiverUrl = "http://10.22.29.41/android_upload.php";
+            Log.v("TAG", "postURL: " + postReceiverUrl);
+
+            // new HttpClient
+            HttpClient httpClient = new DefaultHttpClient();
+
+            // post header
+            HttpPost httpPost = new HttpPost(postReceiverUrl);
+
+            File file = new File(textFile);
+
+
+            FileBody fileBody = new FileBody(file);
+
+            String boundary = "-------------" + System.currentTimeMillis();
+
+            httpPost.setHeader("Content-type", "multipart/form-data; boundary="+boundary);
+
+
+            MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+            reqEntity.addPart("file", fileBody);
+            httpPost.setEntity(reqEntity);
+
+            // execute HTTP post request
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity resEntity = response.getEntity();
+
+            if (resEntity != null) {
+
+                String responseStr = EntityUtils.toString(resEntity).trim();
+                Log.v("TAG", "Response: " +  responseStr);
+
+                // you can add an if statement here and do other actions based on the response
+            }
+
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
